2b288a7fa8bc9adc5d8ed14fa3b22b574541ef94
bipin101 <bipin.dubey@neosofttech.com>
2017-07-07 19:44:24 +0530

    new update LastDEmo act

--- app/src/main/java/com/example/webwerks/lastdemo/LastDemo.java
+++ app/src/main/java/com/example/webwerks/lastdemo/LastDemo.java
@@ -4,5 +4,18 @@ package com.example.webwerks.lastdemo;
  * Created by webwerks on 7/7/17.
  */
 
-public class LastDemo {
+public class LastDemo extends ArrayIndexOutOfBoundsException {
+
+
+    public LastDemo() {
+        super();
+    }
+
+    public LastDemo(int index) {
+        super(index);
+    }
+
+    public LastDemo(String s) {
+        super(s);
+    }
 }
